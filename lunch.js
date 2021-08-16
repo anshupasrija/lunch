8c82904e303d11cbfd0e47d605146b4097e5f819
anshu pasrija <anshu.pasrija@gmail.com>
2021-08-16 22:07:27 +0000

    latest

--- lunch.js
+++ lunch.js
@@ -1,15 +1,28 @@
 const whatToDoForLunch = function (hungry, availableTime) {
-  if (availableTime < 20) {
-    console.log(
-      " Pick something up and eat in back in the Lab or in the kitchen"
-    );
-  } else if (availableTime >= 20 && availableTime < 30) {
-    console.log("Deserve a break and could try a place in Gastown.");
-  } else if (availableTime >= 30) {
-    console.log(
-      "That this is a bootcamp after all and you should probably reconsider."
-    );
-  } else if (!hungry) {
+  if (hungry === false) {
     console.log("Get back to work");
-  } 
+    return "Get back to work";
+  } else if (hungry === true && availableTime < 20) {
+    console.log("Pick something up and eat in lab");
+    return "Pick something up and eat in lab";
+  } else if (hungry === true && availableTime >= 20 && availableTime <= 30) {
+    console.log("Try a nearby place");
+    return "Try a nearby place";
+  } else {
+    return "Please reconsider how much free time you have!";
+  }
 };
+console.log("I'm hungry and I have 20 minutes for lunch.");
+whatToDoForLunch(true, 20);
+console.log("---");
+
+console.log("I'm hungry and I have 50 minutes for lunch.");
+whatToDoForLunch(true, 50);
+console.log("---");
+
+console.log("I'm not hungry and I have 30 minutes for lunch.");
+whatToDoForLunch(false, 30);
+console.log("---");
+
+console.log("I'm hungry and I have 15 minutes for lunch.");
+whatToDoForLunch(true, 15);
